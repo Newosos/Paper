1835751c9a74e917db095cd74cef5df8e966dc69
LENOVO <owenpedagogi@gmail.com>
2025-06-14 12:00:24 -0600

    ver 2 platano

--- Assets/Codigo/Camerafollow.cs
+++ Assets/Codigo/Camerafollow.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//platano
+
 public class Camerafollow : MonoBehaviour
 {
     [SerializeField] Transform player;
